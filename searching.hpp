231d284edd9e9b51c2e2bca05c9cfa1f75f17697
Marshall Clow <marshall@idio.com>
2013-03-13 12:59:18 -0700

    Added copyright and license info

--- searching.hpp
+++ searching.hpp
@@ -1,3 +1,10 @@
+/*
+ (c) Copyright Marshall Clow 2013.
+
+ Distributed under the Boost Software License, Version 1.0.
+ http://www.boost.org/LICENSE_1_0.txt
+*/
+
 #include <algorithm>
 #include <exception>
 #include <vector>
